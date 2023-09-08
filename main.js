b4b321d8614113303435e0249eed09108a3edf64
Kelly <114091619+KelH83@users.noreply.github.com>
2023-09-08 15:45:39 +0100

    First committ

---
+++ main.js
@@ -0,0 +1,10 @@
+let year = document.getElementById("year")
+
+const date = new Date();
+year.textContent = date.getFullYear();
+
+//Search function
+
+function searchFunction(){
+alert("I'm sorry but our search function is currently not working")
+}
