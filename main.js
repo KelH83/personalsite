27141b5a3497fa830dd442cd889293c8927e8830
Kelly <114091619+KelH83@users.noreply.github.com>
2023-11-26 10:03:16 +0000

    Update main.js
    Removed unused code

--- main.js
+++ main.js
@@ -3,8 +3,3 @@ let year = document.getElementById("year")
 const date = new Date();
 year.textContent = date.getFullYear();
 
-//Search function
-
-function searchFunction(){
-alert("I'm sorry but our search function is currently not working")
-}
